393cd47fc4940066462ddc8df1afc079f7d7755a
Fedor Kondratenko <dostoyewski@yandex.ru>
2021-11-10 00:35:14 +0300

    Add blur to fix interpolation points

--- Assets/Scripts/TransferScripts/Images.py
+++ Assets/Scripts/TransferScripts/Images.py
@@ -1,6 +1,6 @@
 import math
 import os
-from math import sin, cos, atan2
+from math import sin, cos
 
 import cv2
 import numpy as np
@@ -65,63 +65,28 @@ class Model(object):
             if max_y < img.shape[1]:
                 max_y = img.shape[1]
         self.shape = (max_x, max_y)
-        self.a = np.full((self.shape[1], int(self.shape[0]), int(self.shape[0])), -1)
+        self.a = np.full((self.shape[0], int(self.shape[1]), int(self.shape[1])), -1)
         self.images = []
         self.construct_images()
-        self.poly = construct_interp_poly([imag.image for imag in self.images[0:3]],
-                                          [float(ang) for ang in self.angles[0:3]])
 
     def construct_images(self):
         for i in range(len(self.angles)):
             if DEBUG:
                 self.images.append(Image(self.names[i], float(self.angles[i])))
 
-    def transform_img(self):
-        for i in tqdm.tqdm(range(len(self.images))):
-            try:
-                n_img = self.images[i] + self.images[i + 1]
-            except IndexError:
-                # Тут создан отдельный класс картинки
-                n_img = self.images[i] + self.images[0]
-            self.process_images([self.images[i], n_img], [-3, -2, -1, 0, 1, 2, 3])
-
     def run_fillament(self):
         for z in tqdm.tqdm(range(len(self.a))):
             for i in range(len(self.a[z])):
                 for j in range(len(self.a[z][i])):
                     params = self.coefs[i][j]
-                    if params['ind0'] < 0 or params['ind0'] >= self.shape[0] or params['ind1'] < 0 or params['ind1'] >= \
-                            self.shape[0]:
+                    if params[2] < 0 or params[2] >= self.shape[1] or params[3] < 0 or params[3] >= self.shape[1]:
                         self.a[z][i][j] = 0
                     else:
-                        w1 = self.images[params['first']].image[z][params['ind0']] * params['a1']
-                        w2 = self.images[params['first']].image[z][params['ind1']] * params['a2']
-                        w3 = self.images[params['second']].image[z][params['ind0']] * params['a3']
-                        w4 = self.images[params['second']].image[z][params['ind1']] * params['a4']
+                        w1 = self.images[params[0]].image[z][params[2]] * params[4]
+                        w2 = self.images[params[0]].image[z][params[3]] * params[5]
+                        w3 = self.images[params[1]].image[z][params[2]] * params[6]
+                        w4 = self.images[params[1]].image[z][params[3]] * params[7]
                         self.a[z][i][j] = w1 + w2 + w3 + w4
-
-    def process_images(self, imgs, offsets=[0]):
-        """
-        Process image with offset
-        :param offsets: array woth offsets
-        :param imgs: array with images
-        :return:
-        """
-        for img in imgs:
-            for offset in offsets:
-                offset_x = offset * sin(img.angle)
-                offset_y = offset * cos(img.angle)
-                for j in range(img.shape[0]):
-                    for k in range(img.shape[1]):
-                        x = round((k - img.offset) * cos(img.angle) + img.offset + offset_x)
-                        y = round((k - img.offset) * sin(img.angle) + img.offset + offset_y)
-                        try:
-                            if self.a[j][y][x] != -1:
-                                self.a[j][y][x] = 0.5 * (self.a[j][y][x] + img.image[j][k])
-                            else:
-                                self.a[j][y][x] = img.image[j][k]
-                        except IndexError:
-                            pass
 
     def get_nearest_img(self, angle):
         for i in range(len(self.angles)):
@@ -132,27 +97,11 @@ class Model(object):
                 return i - 1, i
         return len(self.angles) - 1, 0
 
-    def interp_plate(self):
-        for j in tqdm.tqdm(range(len(self.a[0]))):
-            r, c = np.where(self.a[0] >= 5)
-            for k in range(len(self.a[0][j])):
-                if self.a[0][j][k] == -1:
-                    min_idx = ((r - j) ** 2 + (c - k) ** 2)
-                    d = min_idx.argsort()[0:4]
-                    points = [[r[v], c[v], min_idx[v] ** 0.5] for v in d]
-                    for i in range(len(self.a)):
-                        val = 0
-                        coefs = 0
-                        for point in points:
-                            val += self.a[i][point[0]][point[1]] / point[2]
-                            coefs += 1 / point[2]
-                        self.a[i][j][k] = round(val / coefs)
-
     def calc_coefs(self):
-        coefs = [[{} for j in range(self.shape[0])] for i in range(self.shape[0])]
-        offset = round(self.shape[0] / 2)
-        for i in range(self.shape[0]):
-            for j in range(self.shape[0]):
+        self.coefs = [[{} for j in range(self.shape[1])] for i in range(self.shape[1])]
+        offset = round(self.shape[1] / 2)
+        for i in range(self.shape[1]):
+            for j in range(self.shape[1]):
                 x, y = i - offset, offset - j
                 a = math.atan2(x, y)
                 if a < 0:
@@ -166,14 +115,10 @@ class Model(object):
                     i1 += offset
                     i2 += offset
                 first, second = self.get_nearest_img(a)
-                coefs[i][j]['first'], coefs[i][j]['second'] = first, second
-                coefs[i][j]['ind0'], coefs[i][j]['ind1'] = i1, i2
                 # TODO fix calculation
                 a1, a2, a3, a4 = self.calc_weights(x, y, int(l), int(l) + 1,
                                                    self.angles[first], self.angles[second])
-                coefs[i][j]['a1'], coefs[i][j]['a2'] = a1, a2
-                coefs[i][j]['a3'], coefs[i][j]['a4'] = a3, a4
-        self.coefs = coefs
+                self.coefs[i][j] = [first, second, i1, i2, a1, a2, a3, a4]
 
     def calc_weights(self, x, y, p1, p2, a1, a2):
         """
@@ -191,40 +136,16 @@ class Model(object):
         l3 = ((x - p1 * cos(a2)) ** 2 + (y - p1 * sin(a2)) ** 2) ** 0.5
         l4 = ((x - p2 * cos(a2)) ** 2 + (y - p2 * sin(a2)) ** 2) ** 0.5
         try:
-            c = (l1 * l2 * l3 * l4) / (l1 * l2 * (l3 + l4) + l1 * l3 * l4 + l2 * l3 * l4)
-            return c / l1, c / l2, c / l3, c / l4
+            p = 4
+            a1 = 1 / (l1 ** p)
+            a2 = 1 / (l2 ** p)
+            a3 = 1 / (l3 ** p)
+            a4 = 1 / (l4 ** p)
+            c = 1 / (a1 + a2 + a3 + a4)
+            return c * a1, c * a2, c * a3, c * a4
         except ZeroDivisionError:
             c = 0.5
             return 0, c / l2, 0, c / l4
-
-    def process_transform(self):
-        for j in tqdm.tqdm(range(len(self.a))):
-            for k in range(len(self.a[j][0])):
-                if self.a[j][0][k] == -1:
-                    a, b = self.get_nearest_img(atan2(k, j))
-                    imgs = [self.images[a], self.images[b]]
-                    dists = []
-                    for im in imgs:
-                        dists.extend(im.get_dists(j, k, 0))
-                    dists.sort(key=extract_dist)
-                    points = dists[0:4]
-                    coefs = []
-                    for point in points:
-                        try:
-                            coefs.append(1 / point['dist'])
-                        except ZeroDivisionError:
-                            coefs = [0 for i in range(4)]
-                            coefs[0] = 1
-                            break
-                    kc = 1 / sum(coefs)
-                    for i in range(len(self.a)):
-                        val = 0
-                        for ind, point in enumerate(points):
-                            im = self.get_image_from_angle(point['angle'])
-                            val += im.image[i][point['index']] * kc * coefs[ind]
-                        self.a[j][i][k] = val
-                else:
-                    pass
 
     def get_image_from_angle(self, angle):
         for im in self.images:
@@ -249,44 +170,24 @@ class Model(object):
         self.angles = [float(angle) - first_val for angle in angles]
 
     def save_vertex(self):
-        os.mkdir("./result")
+        foldername = 'result'
+        os.mkdir("./" + foldername)
         print(self.a.shape)
         for i in tqdm.tqdm(range(self.shape[0])):
-            img = self.a[:, i, :]
+            img = self.a[i, :, :]
+            # img = cv2.blur(img, (3, 3))
             if i >= 100:
-                cv2.imwrite("./result/Image" + str(i) + ".png", img)
+                cv2.imwrite("./" + foldername + "/Image" + str(i) + ".png", img)
             elif i < 10:
-                cv2.imwrite("./result/Image00" + str(i) + ".png", img)
+                cv2.imwrite("./" + foldername + "/Image00" + str(i) + ".png", img)
             elif i < 100:
-                cv2.imwrite("./result/Image0" + str(i) + ".png", img)
-
-
-def construct_interp_poly(images, angles):
-    """
-    Constructs 2nd order poly
-    :param images: array with images
-    :param angles: array with corresponding angles
-    :return: interp function
-    """
-    a2 = (images[2] - images[0]) / ((angles[2] - angles[0]) * (angles[2] - angles[1])) - \
-         (images[1] - images[0]) / ((angles[1] - angles[0]) * (angles[2] - angles[1]))
-    a1 = (images[1] - images[0]) / (angles[1] - angles[0]) - a2 * (angles[1] + angles[0])
-    a0 = images[0] - a1 * angles[0] - a2 * angles[0] ** 2
-    return lambda x: a0 + a1 * x + a2 * x ** 2
+                cv2.imwrite("./" + foldername + "/Image0" + str(i) + ".png", img)
 
 
 if __name__ == "__main__":
-    # i = Image("tests/test_2.png", 30)
-    # shape = i.shape
-    # a = np.ndarray((shape[1] + 2, int(1.2 * shape[0]), int(1.2 * shape[0])))
-    # i.get_dists(10, 10, 0)
     m = Model("C:\\Users\\FEDOR\\Documents\\data\\5\\png",
               "C:\\Users\\FEDOR\\Documents\\data\\5\\png\\CaptSave.tag")
     m.calc_coefs()
     m.run_fillament()
-    # m.transform_img()
     print("Image transformed")
-    # m.interp_plate()
-    # m.process_transform()
-    # m.interp_plate()
     m.save_vertex()
